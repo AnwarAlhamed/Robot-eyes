280cb4c2931e439a83e0724f53e8114ee13a1a95
AnwarAlhamed <86317095+AnwarAlhamed@users.noreply.github.com>
2021-08-10 19:51:48 +0300

    Update robot_eyes1.ino

--- robot_eyes1.ino
+++ robot_eyes1.ino
@@ -9,5 +9,5 @@ void loop()
   digitalWrite(6, HIGH);
   delay(50); // Wait for 50 milliseconds
   digitalWrite(6, LOW);
-  delay(50); // Wait for 1000 milliseconds
+  delay(50); // Wait for 50 milliseconds
 }
